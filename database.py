92b24a320c184ac92d514f742b2f2c45362b6a6d
pyrczuu <dydekpyrcz@gmail.com>
2025-01-27 23:23:10 +0100

    prosze dzialaj

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
-from sqlmodel import Session
-from dotenv import load_dotenv
+from sqlmodel import Session, SQLModel
 import os
 
 # if os.getenv("ENVIRONMENT") == "development":
@@ -11,14 +10,14 @@ import os
 #         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 
-load_dotenv()
-
-if os.getenv("ENVIRONMENT") == "development":
-    DATABASE_URL = "sqlite:///./database.db"
-else:
+if os.getenv("ENVIRONMENT") == "production":
     DATABASE_URL = os.getenv("DATABASE_URL")
+else:
+    DATABASE_URL = "sqlite:///./database.db"
 
-engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(DATABASE_URL)
+
+# SQLModel.metadata.create_all(engine)
 
 def get_session():
     with Session(engine) as session:
