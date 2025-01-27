3dcdc2cb01d17ea7348da1b36720daf916e29bb3
pyrczuu <dydekpyrcz@gmail.com>
2025-01-27 23:11:52 +0100

    od marcela wziete sprawdzimy czy dziala

--- database.py
+++ database.py
@@ -1,13 +1,22 @@
 from sqlalchemy import create_engine
 from sqlmodel import Session
+from dotenv import load_dotenv
 import os
 
+# if os.getenv("ENVIRONMENT") == "development":
+#     DATABASE_URL = "sqlite:///./dev.db"
+# else:
+#     DATABASE_URL = os.getenv("DATABASE_URL")
+#     if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+#         DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+
+load_dotenv()
+
 if os.getenv("ENVIRONMENT") == "development":
-    DATABASE_URL = "sqlite:///./dev.db"
+    DATABASE_URL = "sqlite:///./database.db"
 else:
     DATABASE_URL = os.getenv("DATABASE_URL")
-    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
-        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 
