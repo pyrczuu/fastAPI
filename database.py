180854131ae3b4187b81e45a1edc66803b3083d5
pyrczuu <dydekpyrcz@gmail.com>
2025-01-27 23:00:45 +0100

    cos tam zmienione z DATABASE_URL

--- database.py
+++ database.py
@@ -1,7 +1,12 @@
 from sqlalchemy import create_engine
 from sqlmodel import Session
+import os
 
-DATABASE_URL = "sqlite:///./dev.db"
+if os.getenv("ENVIRONMENT") == "development":
+    DATABASE_URL = "sqlite:///./dev.db"
+else:
+    DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 
 def get_session():
