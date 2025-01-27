c49f1a4e609b5bf8d94ef18b0922f1118eb42dcc
pyrczuu <dydekpyrcz@gmail.com>
2025-01-27 23:06:30 +0100

    jakis blad byl

--- database.py
+++ database.py
@@ -6,6 +6,8 @@ if os.getenv("ENVIRONMENT") == "development":
     DATABASE_URL = "sqlite:///./dev.db"
 else:
     DATABASE_URL = os.getenv("DATABASE_URL")
+    if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+        DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(DATABASE_URL, connect_args={'check_same_thread': False})
 
